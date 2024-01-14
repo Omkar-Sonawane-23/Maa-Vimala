55c13a2d65ee1be56c290ddde3a54d4db91c9224
Omkar <sonawaneomkar2306@gmail.com>
2024-01-14 19:56:58 +0530

    Vimala

---
+++ script.js
@@ -0,0 +1,150 @@
+let Input = document.getElementById("input_text");
+let Body = document.getElementById("body");
+let keynum;
+Input.value = "";
+
+Body.addEventListener("keydown", function (e) {
+  Input.focus();
+  if (window.event) {
+    keynum = e.keyCode;
+  }
+  if (keynum == 13) {
+    handleDropDown(Input.value);
+  }
+  Input.addEventListener("input", function (e) {
+    if (Input.value == "") {
+      document.getElementById("content").innerHTML = "";
+    } else {
+      // updateList();
+    }
+  }
+  )
+})
+
+async function showCalcResult(params) {
+  console.log(params);
+  let result = document.getElementById("result");
+  let res = result.innerHTML = "<h1>" + params + "</h1>";
+  console.log(res);
+}
+async function showWordSearchResult(params) {
+  console.log(params);
+  let result = document.getElementById("result");
+  if (params.title != "No Definitions Found") {
+    let res = result.innerHTML = "<h1>" + params[0].meanings[0].definitions[0].definition + "</h1>";
+  } else {
+    let res = result.innerHTML = "<h1>" + params.title + "</h1>";
+  }
+  console.log(res);
+}
+async function showWikiResult(params) {
+  let result = document.getElementById("result");
+  result.innerHTML = "";
+  let list = params.query.search;
+  result.innerHTML = "<h1>Wikipedia Search Results</h1>";
+  list.map((item, i) => {
+    let res = result.innerHTML += `<li class="wiki-result"><a onclick="loadwiki(${list[i].pageid})">` + list[i].title + "</a></li>";
+  }
+  )
+  console.log(res);
+}
+function loadwiki(params) {
+  let result = document.getElementById("result");
+  result.innerHTML = "";
+  result.innerHTML = `<iframe src="https://en.wikipedia.org/?curid=${params}" width="1500vw" height="900px"></iframe>`;
+}
+
+function handleDropDown(search) {
+  if (search.includes("*") || search.includes("-") || search.includes("/") || search.includes("+")) {
+    showCalcResult(performCalculation(search));
+  } else if (hasOnlyOneWord(search)) {
+    console.log("Word Search");
+    performWordSearch(search)
+  } else if (search.includes("set") || search.includes("add") || search.includes("todo")) {
+    console.log("Set Todo");
+    performTodoSet(search);
+  } else if (search.includes("remove") || search.includes("delete") || search.includes("remove todo")) {
+    console.log("remove Todo");
+    performToDoUpdate(search);
+  } else if (search.includes("google") || search.includes("search google") || search.includes("how") || search.includes("what") || search.includes("why") || search.includes("when") || search.includes("where") || search.includes("who") || search.includes("which") || search.includes("whom") || search.includes("whose") || search.includes("google search")) {
+    performGoogleSearch(search);
+  } else if (search.includes("youtube") || search.includes("search youtube") || search.includes("youtube search")) {
+    performYouTubeSearch(search);
+  } else if (search.includes("wikipedia") || search.includes("search wikipedia") || search.includes("wikipedia search")) {
+    performWikipediaSearch(search);
+  }
+}
+function hasOnlyOneWord(inputString) {
+  var wordRegex = /^\w+$/;
+  return wordRegex.test(inputString);
+}
+function performCalculation(expression) {
+  var calculate = new Function('return ' + expression);
+  var result = calculate();
+  return result;
+}
+function performWordSearch(word) {
+  let result = fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`).then(response => {
+    response.json().then(data => {
+      showWordSearchResult(data);
+    })
+  })
+}
+function performTodoSet(cmd) {
+  let res = cmd.split("add ");
+  let alltodo = JSON.parse(localStorage.getItem("todolist")) || [];
+  let newTodo = res[1].trim();
+  alltodo.push(newTodo);
+  console.log(alltodo);
+  localStorage.setItem("todolist", JSON.stringify(alltodo));
+}
+function performToDoUpdate(cmd) {
+  let res = cmd.split("remove ");
+  let alltodoRemove = JSON.parse(localStorage.getItem("todolist")) || [];
+  let indexToRemove = alltodoRemove.indexOf(res[1]);
+  if (indexToRemove !== -1) {
+    alltodoRemove.splice(indexToRemove, 1);
+    localStorage.setItem("todolist", JSON.stringify(alltodoRemove));
+  } else {
+    console.log("The word to remove is not in the list.");
+  }
+}
+function performGoogleSearch(search) {
+  document.location = `https://www.google.com/search?q=${encodeURIComponent(search)}`
+  search = "";
+}
+function performYouTubeSearch(search) {
+  let res = search.split("youtube");
+  document.location = `https://www.youtube.com/results?search_query=${encodeURIComponent(res[1].trim())}`
+  search = "";
+}
+function performDuckDuckGoSearch(search) {
+  document.location = `https://duckduckgo.com/?q=${encodeURIComponent(search)}`
+  search = "";
+}
+function performBingSearch(search) {
+  document.location = `https://www.bing.com/search?q=${encodeURIComponent(search)}`
+  search = "";
+}
+function performWolframAlphaSearch(search) {
+  document.location = `https://www.wolframalpha.com/input/?i=${encodeURIComponent(search)}`
+  search = "";
+}
+function performWikipediaSearch(search) {
+  fetch(`https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=5&srsearch=${encodeURIComponent(search)}`).then(response => { response.json().then(data => { 
+    console.log(data);
+    showWikiResult(data) ;
+  }) })
+  search = "";
+}
+function performWhetherCheck(search) {
+  search = "";
+}
+function performImageSearchOnPexels(search) {
+  document.location = `https://www.pexels.com/?s=${encodeURIComponent(search)}`
+  search = "";
+}
+function performImageSearchOnUnsplashes(search) {
+  document.location = `https://unsplash.com/s/photos/${encodeURIComponent(search)}`
+  search = "";
+}
